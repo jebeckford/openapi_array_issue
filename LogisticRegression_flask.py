0d6d2b20ee734d5b0ad25d2501ac6fe8f488d18f
jebeckford <jebeckford@gmail.com>
2020-04-30 21:44:16 -0400

    Update and rename LogisticRegression_cmsoaserver.py to LogisticRegression_flask.py

--- LogisticRegression_flask.py
+++ LogisticRegression_flask.py
@@ -2,10 +2,10 @@
 import connexion
 
 # Create the application instance
-app = connexion.App(__name__, specification_dir='<wherever LogisticRegression.py is located>')
+app = connexion.App(__name__, specification_dir='./')
 
 # Read the yaml file to configure the endpoints
-app.add_api('/<whatever directory>/LogisticRegression.yaml')
+app.add_api('./LogisticRegression.yaml')
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1',
